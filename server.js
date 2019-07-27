25c32feb1390cd2978bcb68f403903b699c83e3d
ajkim41 <ajkim41@gmail.com>
2019-07-27 17:21:01 -0400

    Update Server

--- server.js
+++ server.js
@@ -12,8 +12,10 @@ app.use(logger("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
+var MONGODB_URI =
+  process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 
-mongoose.connect("mongodb://localhost/newsScrape", {
+mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true
 });
 
